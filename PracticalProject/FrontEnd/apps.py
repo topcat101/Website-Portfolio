c2eca04423c39d6ae21893afbe6cb40b86f8efd6
topcat101 <thomascwood1997@gmail.com>
2025-06-24 19:34:21 +0100

    Changed name of file from CrudWebApp to FrontEnd

--- PracticalProject/FrontEnd/apps.py
+++ PracticalProject/FrontEnd/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CrudwebappConfig(AppConfig):
+class FrontEndConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CrudWebApp'
+    name = 'FrontEnd'
