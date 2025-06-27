7cdcfbc059b0698ba8027bce9ab6c5202f8ce708
topcat101 <thomascwood1997@gmail.com>
2025-06-27 17:26:01 +0100

    Changed from .html to just fullname

--- PracticalProject/FrontEnd/urls.py
+++ PracticalProject/FrontEnd/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.Home, name="Home"),
-    path("index.html", views.Home, name="Home"),
-    path("About.html", views.About, name="About"),
-    path("Contact.html", views.Contact, name="Contact"),
-    path("Projects.html", views.Projects, name="Projects"),
-    path("Login.html", views.Login, name="Login"),
-    path("SignUp.html", views.SignUp, name="SignUp")
+    path("Home", views.Home, name="Home"),
+    path("About", views.About, name="About"),
+    path("Contact", views.Contact, name="Contact"),
+    path("Projects", views.Projects, name="Projects"),
+    path("Login", views.Login, name="Login"),
+    path("SignUp", views.SignUp, name="SignUp")
 ]
--- PracticalProject/FrontEnd/views.py
+++ PracticalProject/FrontEnd/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, HttpResponse
 
 def Home(request):
-    return render(request, "index.html")
+    return render(request, "Home")
 
 def About(request):
-    return render(request, "About.html")
+    return render(request, "About")
 
 def Contact(request):
-    return render(request, "Contact.html")
+    return render(request, "Contact")
 
 def Login(request):
-    return render(request, "Login.html")
+    return render(request, "Login")
 
 def SignUp(request):
-    return render(request, "SignUp.html")
+    return render(request, "SignUp")
 
 def Projects(request):
-    return render(request, "SignUp.html")
+    return render(request, "Projects")
