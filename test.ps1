f5f058a06e4f95381de73b235ca1ae19aea59999
topcat101 <thomascwood1997@gmail.com>
2025-07-11 01:06:57 +0100

    Add text

--- test.ps1
+++ test.ps1
@@ -1,3 +1,5 @@
+#This file push code to the requested github for Windows systems only
+
 Write-Host "Please enter the name of the commit"
 $commit_name = Read-Host "Waiting for user's input"
 Write-Host "The name of the commit $($commit_name)"
